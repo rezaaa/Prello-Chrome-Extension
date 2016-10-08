2e2ff94ad14075f3a187e7016474082197d04788
Mohammad Reza <rezamahmudy1@gmail.com>
2016-10-08 21:49:59 +0330

    concat scripts / add watch task to gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,17 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+var gulp   = require('gulp');
+var babel  = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('run', function() {
-  gulp.src('./src/*.js')
-  .pipe(babel({presets: ['es2015']}))
-  .pipe(gulp.dest('./'))
+  gulp.src(['./src/*.js'])
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function(){
+  gulp.run('run');
+  gulp.watch('./src/*.js', ['run']);
 });
